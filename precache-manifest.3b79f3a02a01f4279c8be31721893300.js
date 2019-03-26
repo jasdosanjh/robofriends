ccf0de6d40b1e00fe1d3fb09ff8d07cc97c7fb12
jazdosanjh <jas@dosanjh.co.uk>
2019-03-26 18:19:01 +0000

    Updates

---
+++ precache-manifest.3b79f3a02a01f4279c8be31721893300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "fa97e6406b552b5e9dc3",
+    "url": "/robofriends/static/js/main.622c46c8.chunk.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/robofriends/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "fa97e6406b552b5e9dc3",
+    "url": "/robofriends/static/css/main.4d0ce592.chunk.css"
+  },
+  {
+    "revision": "5f3bc86c140273ce6ad800f7a87453c4",
+    "url": "/robofriends/index.html"
+  }
+];
