3c22a5b5000649e1bd285c05724139de52422df4
jazdosanjh <jas@dosanjh.co.uk>
2019-03-26 17:31:17 +0000

    Replaced dummy robot data with a online REST API for testing and prototyping via the fetch method. I've also added a simple loading message, if the the app is unable to fetch any data.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ""
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users }));
     }
 
     onSearchChange = (event) => {
@@ -21,6 +26,9 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
         return (
             <div className="tc">
                 <h1 className="f1">RoboFriends</h1>
@@ -29,6 +37,6 @@ class App extends Component {
             </div>
         );
     }
-}
+}}
 
 export default App;
