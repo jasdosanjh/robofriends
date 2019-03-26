d1620f65bec746b2fed86c996325a3746b4cc47a
jazdosanjh <jas@dosanjh.co.uk>
2019-03-26 13:23:33 +0000

    Imported tachyons css for fast prototyping styling, and created the card components using the robohash API to fetch random images of robots for each card.

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Ned Stark',
+        username: 'Ned',
+        email: 'nedstark@gameofthrones.com'
+    },
+    {
+        id: 2,
+        name: 'Rob Stark',
+        username: 'Rob',
+        email: 'robstark@gameofthrones.com'
+    },
+    {
+        id: 3,
+        name: 'Benji Stark',
+        username: 'Benji',
+        email: 'benjistark@gameofthrones.com'
+    },
+    {
+        id: 4,
+        name: 'Arya Stark',
+        username: 'Arya',
+        email: 'aryastark@gameofthrones.com'
+    },
+    {
+        id: 5,
+        name: 'Jamie Lannister',
+        username: 'Jamie',
+        email: 'jamielannister@gameofthrones.com'
+    },
+    {
+        id: 6,
+        name: 'Tyrion Lannister',
+        username: 'Tyrion',
+        email: 'tyrionlannister@gameofthrones.com'
+    },
+    {
+        id: 7,
+        name: 'Jon Snow',
+        username: 'Jon',
+        email: 'jonsnow@gameofthrones.com'
+    },
+    {
+        id: 8,
+        name: 'Khal Drogo',
+        username: 'Drogo',
+        email: 'khaldrogo@gameofthrones.com'
+    },
+    {
+        id: 9,
+        name: 'The Hound',
+        username: 'Hound',
+        email: 'thehound@gameofthrones.com'
+    },
+    {
+        id: 10,
+        name: 'The Mountain',
+        username: 'Mountain',
+        email: 'themountain@gameofthrones.com'
+    },
+]
